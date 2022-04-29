8e7329197b321e1fae7bcc816fb07f0fffabaddc
Hagen E. Fritz <hagenfritz@gmail.com>
2022-04-29 00:57:11 -0500

    reworked

--- src/generate_report.py
+++ src/generate_report.py
@@ -1,9 +1,12 @@
 import os, sys
-from tabnanny import verbose
+import argparse
+
 import pandas as pd, numpy as np, jinja2, pdfkit
 from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import logging
+import pathlib
 
 class Report:
 
@@ -27,36 +30,58 @@ class Report:
         previous_date : datetime.date
             previous report date
         """
-        self.data = pd.read_excel(f"~/Documents/volleyball/data/pickup_stats_{date_str}.xlsx", parse_dates=['date'])
+        # file paths
+        self.path_to_this_dir = f"{pathlib.Path(__file__).resolve().parent}"
+        self.project_dir = f"{pathlib.Path(__file__).resolve().parent.parent}" # taking advantage of project filesystem
+
+        # setting up logging
+        self.logger = logging.getLogger(__name__)
+        log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        logging.basicConfig(level=logging.INFO, filename=f'{self.path_to_this_dir}/report.log', filemode='w', format=log_fmt,datefmt='%Y/%m/%d %H:%M:%S')
+
+        # Data
+        # ----
+        # getting current, specified data
+        self.data = pd.read_excel(f"{self.project_dir}/data/pickup_stats_{date_str}.xlsx", parse_dates=['date'])
         self.data.dropna(subset=['date'], inplace=True)
         self.date = datetime.strptime(date_str, '%m%d%Y').date()
+        self.total_games = int(len(self.data)/4)
+        
+        # getting previous data
         previous_file_date = datetime(2000, 1, 1).date()
-        for file in os.listdir('/Users/hagenfritz/Documents/volleyball/data/'):
-            str_from_file = file.split('_')[(-1)].split('.')[0]
-            try:
-                file_date = datetime.strptime(str_from_file, '%m%d%Y').date()
-                if file_date < self.date:
-                    if file_date > previous_file_date:
-                        previous_file_date = file_date
-            except ValueError:
-                pass
+        for file in os.listdir(f"{self.project_dir}/data/"): # looping through all previous files
+            if file[-1] in ["v","x"]: # looking for XSLX or CSV files
+                str_from_file = file.split('_')[(-1)].split('.')[0] 
+                try:
+                    file_date = datetime.strptime(str_from_file, '%m%d%Y').date()
+                    if file_date < self.date:
+                        if file_date > previous_file_date:
+                            previous_file_date = file_date # updating to a later file and looking again
+                        else:
+                            self.previous_date = previous_file_date
+                            previous_date_str = datetime.strftime(previous_file_date, '%m%d%Y')
+                            self.previous_data = pd.read_excel(f"{self.project_dir}/data/pickup_stats_{previous_date_str}.xlsx", parse_dates=['date'])
+                            self.previous_data.dropna(subset=['date'], inplace=True)
+                except ValueError as e:
+                    self.logger.exception(e)
 
-        else:
-            self.previous_date = previous_file_date
-            previous_date_str = datetime.strftime(previous_file_date, '%m%d%Y')
-            self.previous_data = pd.read_excel(f"~/Documents/volleyball/data/pickup_stats_{previous_date_str}.xlsx", parse_dates=['date'])
-            self.previous_data.dropna(subset=['date'], inplace=True)
-            n_games_per_player = self.data.groupby('name').count()
-            single_game_players = []
-            for player in n_games_per_player.index:
-                if n_games_per_player.loc[(player, 'win_loss')] == 1:
-                    single_game_players.append(player)
-                print('Removing the following players since they only have one game:')
-                for player in single_game_players:
-                    print(f"\t{player}")
-                else:
-                    self.data = self.data[(~self.data['name'].isin(single_game_players))]
-                    self.previous_data = self.previous_data[(~self.previous_data['name'].isin(single_game_players))]
+        # Players with only a few games
+        # --------------------------------------
+        ## Getting the Players
+        n_games_per_player = self.data.groupby('name').count()
+        low_game_players = []
+        for player in n_games_per_player.index:
+            if n_games_per_player.loc[(player, 'win_loss')] < 0.05 * self.total_games: # have to have played in at least 5% of the games
+                low_game_players.append(player)
+        
+        ## Logging the Low-Playing Players
+        self.logger.warning('Removing the following players since they only have one game:')
+        for player in low_game_players:
+            self.logger.warning(f"\t{player}")
+
+        ## Removing them from both datasets
+        self.data = self.data[(~self.data['name'].isin(low_game_players))]
+        self.previous_data = self.previous_data[(~self.previous_data['name'].isin(low_game_players))]
 
     def calculate_win_rate(self, player, latest=True):
         """
@@ -66,6 +91,8 @@ class Report:
         ----------
         player : str
             name of the player
+        latest : boolean, default True
+            whether to use the latest data or the previous data
 
         Returns
         -------
@@ -74,10 +101,12 @@ class Report:
         n_games : int
             number of games the player has participated in
         """
+        # data version control
         if latest:
             data = self.data.copy()
         else:
             data = self.previous_data.copy()
+
         player = player.title()
         player_data = data[(data['name'] == player)]
         n_games = len(player_data)
@@ -85,6 +114,7 @@ class Report:
             win_rate = round(len(player_data[(player_data['win_loss'] == 'win')]) / n_games * 100, 1)
         else:
             win_rate = 0
+
         return (win_rate, n_games)
 
     def calculate_ace_error_ratio(self, player, latest=True):
@@ -95,6 +125,8 @@ class Report:
         ----------
         player : str
             name of the player
+        latest : boolean, default True
+            whether to use the latest data or the previous data
 
         Returns
         -------
@@ -128,38 +160,32 @@ class Report:
             data = self.data.copy()
         else:
             data = self.previous_data.copy()
-        per_game = {[]:col for col in data.columns if col not in ('date', 'partner',
-                                                                  'win_loss', 'match_id',
-                                                                  'tournament') if col not in ('date',
-                                                                                               'partner',
-                                                                                               'win_loss',
-                                                                                               'match_id',
-                                                                                               'tournament')}
-        for calculated_stat in ('win_rate', 'ace2error'):
-            per_game[calculated_stat] = []
-        else:
-            per_game['n'] = []
-            for player in data['name'].unique():
-                player_data = data[(data['name'] == player)]
-                for key in per_game.keys():
-                    if key == 'name':
-                        per_game[key].append(player)
-                    elif key == 'win_rate':
-                        win_rate, _ = self.calculate_win_rate(player, latest=latest)
-                        per_game['win_rate'].append(win_rate)
-                    elif key == 'ace2error':
-                        ace2error = self.calculate_ace_error_ratio(player, latest=latest)
-                        per_game['ace2error'].append(ace2error)
-                    elif key == 'n':
-                        per_game['n'].append(len(player_data))
-                    elif key in ('hitting_efficiency', 'serving_percentage'):
-                        per_game[key].append(round(np.nanmean(player_data[key]) * 100, 1))
-                    else:
-                        per_game[key].append(round(np.nanmean(player_data[key]), 1))
-                else:
-                    return pd.DataFrame(per_game)
 
-    def get_winningest_team(self, min_games=2, top_teams=5):
+        per_game = {col: [] for col in data.columns if col not in ['date', 'partner','win_loss', 'match_id','tournament',"switch1","switch2","switch3","switch4","switch5","switch6","switch7","switch8"]}
+        for calculated_stat in ('n','win_rate', 'ace2error'):
+            per_game[calculated_stat] = []
+
+        for player in data['name'].unique():
+            player_data = data[(data['name'] == player)]
+            for key in per_game.keys():
+                if key == 'name':
+                    per_game[key].append(player)
+                elif key == 'win_rate':
+                    win_rate, _ = self.calculate_win_rate(player, latest=latest)
+                    per_game['win_rate'].append(win_rate)
+                elif key == 'ace2error':
+                    ace2error = self.calculate_ace_error_ratio(player, latest=latest)
+                    per_game['ace2error'].append(ace2error)
+                elif key == 'n':
+                    per_game['n'].append(len(player_data))
+                elif key in ('hitting_efficiency', 'serving_percentage'):
+                    per_game[key].append(round(np.nanmean(player_data[key]) * 100, 1))
+                else:
+                    per_game[key].append(round(np.nanmean(player_data[key]), 1))
+                    
+        return pd.DataFrame(per_game)
+
+    def get_winningest_team(self, min_games=5, top_teams=5):
         """
         Gets the team with the most victories
 
@@ -229,14 +255,20 @@ class Report:
         """
         Gets the corresponding arrow
         """
-        print(f"/Users/hagenfritz/Documents/volleyball/images/{color}_{direction}.png")
-        return f"/Users/hagenfritz/Documents/volleyball/images/{color}_{direction}.png"
+        print(f"{self.project_dir}/images/{color}_{direction}.png")
+        return f"{self.project_dir}/images/{color}_{direction}.png"
 
-    def compare_stat(self, latest):
+    def compare_stats(self):
         """
         
         """
-        
+        latest_stats = self.calculate_per_game_stats(latest=True)
+        previous_stats = self.calculate_per_game_stats(latest=False)
+        latest_stats.set_index("name",inplace=True)
+        previous_stats.set_index("name",inplace=True)
+
+        changes = latest_stats - previous_stats
+        return changes.round(decimals=1)
 
     def plot_stats_over_time(self, player):
         """
@@ -272,14 +304,14 @@ class Report:
             for loc in ('top', 'right'):
                 ax.spines[loc].set_visible(False)
             else:
-                plt.savefig(f"/Users/hagenfritz/Documents/volleyball/figures/{player}-stats_over_time.png")
+                plt.savefig(f"{self.project_dir}/figures/{player}-stats_over_time.png")
                 plt.close()
 
     def get_player_figure(self, player):
         """
         
         """
-        return f"/Users/hagenfritz/Documents/volleyball/figures/{player}-stats_over_time.png"
+        return f"{self.project_dir}/figures/{player}-stats_over_time.png"
 
     def run(self, n_top_players=5):
         """
@@ -290,58 +322,85 @@ class Report:
         n_top_players : int, default 5
             number of players to include on the leaderboard
         """
+        self.logger.info("Calculating per game statistics")
         stats_per_game = self.calculate_per_game_stats(latest=True)
+
+        self.logger.info("Getting leaderboard stats")
+        results = {}
         top_results = {}
+        bottom_results = {}
+        mip_results = {}
+        changes = self.compare_stats()
         for variable in stats_per_game.columns:
             if variable not in ('name', ):
                 var_results = {}
+                top_results = {}
+                bottom_results = {}
+                mip_results = {}
                 if variable in ('errors', ):
                     ascend = True
                 else:
                     ascend = False
+                # top results
                 top_results_per_variable = stats_per_game.sort_values(variable, ascending=ascend)[:n_top_players]
                 for player, value in zip(top_results_per_variable['name'].values, top_results_per_variable[variable].values):
-                    var_results[player] = value
-                else:
-                    top_results[variable] = var_results
+                    top_results[player] = value
 
-            for player in stats_per_game['name'].unique():
-                self.plot_stats_over_time(player)
-            else:
-                templateLoader = jinja2.FileSystemLoader(searchpath='/Users/hagenfritz/Documents/volleyball/templates/')
-                templateEnv = jinja2.Environment(loader=templateLoader)
-                func_dict = {'compare_stat':self.compare_stat, 
-                 'get_player_figure':self.get_player_figure}
-                TEMPLATE_FILE = 'stat_update_template.html'
-                template = templateEnv.get_template(TEMPLATE_FILE)
-                template.globals.update(func_dict)
-                outputText = template.render(date=(self.date), previous_date=(self.previous_date), n_games=(int(len(self.data) / 4)),
-                  win_rate=(top_results['win_rate']),
-                  effectiveness=(top_results['effectiveness']),
-                  kills=(top_results['kills']),
-                  efficiency=(top_results['hitting_efficiency']),
-                  aces=(top_results['aces']),
-                  serving=(top_results['serving_percentage']),
-                  blocks=(top_results['blocks']),
-                  errors=(top_results['errors']),
-                  pass_rating=(top_results['serve_receive_rating']),
-                  ace2error=(top_results['ace2error']),
-                  top_teams=self.get_winningest_team(top_teams=n_top_players),
-                  per_player=(self.get_simplified_results_per_player()))
-                html_file = open(f"/Users/hagenfritz/Documents/volleyball/reports/bollyvall_report-{self.date}.html", 'w')
-                html_file.write(outputText)
-                html_file.close()
-                pdfkit.from_file(f"/Users/hagenfritz/Documents/volleyball/reports/bollyvall_report-{self.date}.html", f"/Users/hagenfritz/Documents/volleyball/reports/bollyvall_report-{self.date}.pdf")
+                var_results["top"] = top_results
+                
+                # bottom results
+                bot_results_per_variable = stats_per_game.sort_values(variable, ascending=not ascend)[:n_top_players]
+                for player, value in zip(bot_results_per_variable['name'].values, bot_results_per_variable[variable].values):
+                    bottom_results[player] = value
+                
+                var_results["bottom"] = bottom_results
+
+                # mip results
+                mip_results_per_variable = changes.sort_values(variable, ascending=ascend)[:n_top_players]
+                for player, value in zip(mip_results_per_variable.index.values, mip_results_per_variable[variable].values):
+                    mip_results[player] = value
+
+                var_results["mip"] = mip_results
+
+                results[variable] = var_results
+
+        self.results = results
+
+        for player in stats_per_game['name'].unique():
+            self.plot_stats_over_time(player)
+
+        templateLoader = jinja2.FileSystemLoader(searchpath=f'{self.project_dir}/templates/')
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        func_dict = {'compare_stat':self.compare_stats, 
+            'get_player_figure':self.get_player_figure}
+        TEMPLATE_FILE = 'stat_update_template.html'
+        template = templateEnv.get_template(TEMPLATE_FILE)
+        template.globals.update(func_dict)
+        outputText = template.render(date=self.date, previous_date=self.previous_date, n_games=self.total_games,
+            win_rate=results['win_rate'],
+            effectiveness=results['effectiveness'],
+            kills=results['kills'],
+            efficiency=results['hitting_efficiency'],
+            aces=results['aces'],
+            serving=results['serving_percentage'],
+            blocks=results['blocks'],
+            errors=results['errors'],
+            pass_rating=results['serve_receive_rating'],
+            ace2error=results['ace2error'],
+            top_teams=self.get_winningest_team(top_teams=n_top_players),
+            per_player=self.get_simplified_results_per_player())
+        html_file = open(f"{self.project_dir}/reports/hlb_report-{self.date}.html", 'w')
+        html_file.write(outputText)
+        html_file.close()
+        pdfkit.from_file(f"{self.project_dir}/reports/hlb_report-{self.date}.html", f"{self.project_dir}/reports/hlb_report-{self.date}.pdf")
 
 if __name__ == '__main__':
-    try:
-        date_str = sys.argv[1]
-        try:
-            n_top_players = sys.argv[2]
-        except IndexError:
-            n_top_players = 5
-        else:
-            report = Report(date_str)
-            report.run(n_top_players=n_top_players)
-    except IndexError:
-        print('Need to include valid date string')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', help='date of report - should be string in format %m%d%Y', default='04272022',type=str)    
+    parser.add_argument('-n', help='number players to use in leaderboard.', default=5, type=int) 
+    args = parser.parse_args()
+
+    # Generating the Report
+    # ---------------------
+    report = Report(args.d)
+    report.run(args.n)
